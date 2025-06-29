4361071515634239d098a7d7e5e7d9817ad1a77b
jai-cmyk <jailavakusa97@gmail.com>
2025-06-30 02:12:55 +0530

    Deleted package-lock.json

--- backend/services/shared/src/index.js
+++
@@ -1,8 +0,0 @@
-// Shared utilities and types for FlowForge services
-module.exports = {
-  Database: require('./utils/database').Database,
-  RedisClient: require('./utils/redis').RedisClient,
-  logger: require('./utils/logger'),
-  authMiddleware: require('./middleware/auth'),
-  validationMiddleware: require('./middleware/validation')
-};
