356aa1aad299d6d0cae56544f750525ed238c6f0
jai-cmyk <jailavakusa97@gmail.com>
2025-07-06 00:08:08 +0530

    Updated validation.js

--- backend/src/middleware/validation.js
+++ backend/src/middleware/validation.js
@@ -5,6 +5,7 @@ const validateBody = (schema) => {
   return (req, res, next) => {
     console.log(schema,req.body)
     const { error, value } = schema.validate(req.body);
+    console.log(error)
     if (error) {
       return res.status(400).json({
         success: false,
